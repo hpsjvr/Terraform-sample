6f4df1bdc2ebb04a9e188bfff0df6507a2e7a817
hpsjvr <99760009+hpsjvr@users.noreply.github.com>
2024-01-31 21:29:46 +0530

    Create variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "network_name" {
+  description = "The name of the network being created"
+  type        = string
+}
+variable "project_id" {
+  description = "The ID of the project where this VPC will be created"
+  type        = string
+}
